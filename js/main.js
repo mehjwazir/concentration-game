a111a11bb4bb60244cd16a2459445d0770387781
mehjwazir <mehjwazir@gmail.com>
2022-01-03 16:53:44 -0800

    test commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+/*----- constants -----*/
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	const cardArray = [
+		{
+			name: '',
+			img: ''
+		}
+	]
+})
+
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
